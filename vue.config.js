3db3be0ec1917a856e9221e919eff3aa9eb49551
AVB <alexander.bilichenko@gmail.com>
2021-07-08 14:38:14 +0300

    Reverted vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,6 @@
-const DEBUG_PRODUCTION = true;
-
 module.exports = {
-    filenameHashing: !DEBUG_PRODUCTION,
     publicPath: process.env.NODE_ENV === 'production'
-        ? (DEBUG_PRODUCTION ? 'http://localhost:8080/' : '/v2/')
+        ? '/v2/'
         : '/',
     outputDir: '../public/v2/'//,
     //indexPath: '../../templates/html_v2.php'
